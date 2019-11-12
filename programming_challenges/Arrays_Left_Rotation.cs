421df788d69a8b33875bf692e199baa8f6f14658
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2019-11-12 14:09:53 -0800

    New solutions

---
+++ programming_challenges/Arrays_Left_Rotation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace programming_challenges
+{
+    class Arrays_Left_Rotation
+    {
+        // Complete the rotLeft function below.
+        static int[] rotLeft(int[] a, int d)
+        {
+            int[] rotArray = new int[a.Length];
+
+            if (d > a.Length)
+            {
+                d = d - a.Length;
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i + d < a.Length)
+                {
+                    rotArray[i] = a[i + d];
+                }
+                else
+                {
+                    rotArray[i] = a[i + d - a.Length];
+                }
+            }
+
+            return rotArray;
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nd = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nd[0]);
+
+            int d = Convert.ToInt32(nd[1]);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+            ;
+            int[] result = rotLeft(a, d);
+
+            Console.WriteLine(string.Join(" ", result));
+
+            //textWriter.WriteLine(string.Join(" ", result));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
---
+++ programming_challenges/New_Year_Chaos.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace programming_challenges
+{
+    class New_Year_Chaos
+    {
+        // Complete the minimumBribes function below.
+        static void minimumBribes(int[] q)
+        {
+            int noOfBribes = 0;
+           
+            for (int i = q.Length - 1; i>0; i--)
+            {
+                //Console.WriteLine(i);
+                if (q[i] != i+1)
+                {
+                    if((i-1>=0) && q[i-1] == i + 1)
+                    {
+                        noOfBribes++;
+                        //Console.WriteLine("111 " + i);
+                        Swap(q, i, i - 1);
+                        //Console.WriteLine(string.Join(",", q));
+                        //Console.WriteLine("noOfBribes "+ noOfBribes);
+                    }
+                    else if ((i - 2 >= 0) && q[i - 2] == i + 1)
+                    {
+                        noOfBribes+=2;
+                        //Console.WriteLine("222 " + i);
+                        Swap(q, i-1, i - 2);
+                        //Console.WriteLine(string.Join(",", q));
+                        Swap(q, i, i - 1);
+                        //Console.WriteLine(string.Join(",", q));
+                        //Console.WriteLine("noOfBribes " + noOfBribes);
+                    }
+                    else 
+                    {
+                        //Console.WriteLine("kkk " + i);
+                        noOfBribes = -1;
+                        break;
+
+                    }
+
+                }
+                   
+            }
+            if (noOfBribes == -1)
+            {
+                Console.WriteLine("Too chaotic");
+            }
+            else
+            {
+                Console.WriteLine(noOfBribes);
+            }
+            
+        }
+
+        static void Swap(int[] a, int i, int j)
+        {
+            int temp = a[j];
+            a[j] = a[i];
+            a[i] = temp;
+                         
+        }
+
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp));
+                minimumBribes(q);
+            }
+        }
+    }
+}
